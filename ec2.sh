ba08d8bdea881e231998754adc73e57668a1d05e
Mohammad Efazati <mohammad@efazati.org>
2019-10-03 12:43:10 +0100

    Add EC2 monitor in bash

---
+++ ec2.sh
@@ -0,0 +1,22 @@
+declare -a environments=("development" "staging" "production")
+for environment in "${environments[@]}"
+do
+    echo "${environment}:"
+    for region in $(aws ec2 describe-regions --profile "${environment}" --query 'Regions[].RegionName | sort(@)' --output text); \
+    do 
+        nodes=$(aws ec2 describe-instances \
+            --region "${region}" \
+            --filter Name=instance-state-name,Values=running  --profile "${environment}"  \
+            --query 'Reservations[].Instances[].{InstanceType:InstanceType}' \
+            | jq -r '.[].InstanceType' \
+            | sort \
+            | uniq -c \
+            | sort -r)
+
+        if  [ ! -z "$nodes" ]
+        then
+            echo "   ${region}"
+            echo "${nodes}"
+        fi
+    done
+done
