e8af5f204d88efab716ddf9c2e2f9d2748c79e82
Mohammad Efazati <mohammad@efazati.org>
2019-10-18 11:11:00 +0100

    Add network intefaces count

--- ec2.sh
+++ ec2.sh
@@ -3,6 +3,7 @@ do
     echo "${environment}:"
     for region in $(aws ec2 describe-regions --profile "${environment}" --query 'Regions[].RegionName | sort(@)' --output text); \
     do 
+
         nodes=$(aws ec2 describe-instances \
             --region "${region}" \
             --filter Name=instance-state-name,Values=running  --profile "${environment}"  \
@@ -14,7 +15,9 @@ do
 
         if  [ ! -z "$nodes" ]
         then
+            network_intefaces=$(aws ec2 describe-network-interfaces --profile "${environment}" --region "${region}" -- 2>&1 | jq '.NetworkInterfaces | .[] | "\(.NetworkInterfaceId) -> \(.Description)"'  | wc -l)
             echo "   ${region}"
+            echo "    * Network intefaces: ${network_intefaces}"
             echo "${nodes}"
         fi
     done
