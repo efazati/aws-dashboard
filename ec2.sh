48b4626091f515ef28720092ef6ae3c072098c28
Mohammad Efazati <mohammad@efazati.org>
2019-10-18 12:47:47 +0100

    List the VPCs and CIDR below each region

--- ec2.sh
+++ ec2.sh
@@ -22,11 +22,15 @@ do
             | wc -l)
         network_intefaces=$(aws ec2 describe-network-interfaces --profile "${environment}" --region "${region}" 2>&1 | jq '.NetworkInterfaces | .[] | "\(.NetworkInterfaceId) -> \(.Description)"'  | wc -l)
         public_ips=$(aws ec2 describe-addresses --profile "${environment}" --region "${region}" 2>&1 | jq '.Addresses | .[] | .PublicIp' | wc -l)
+        vpc_cidr=$(aws ec2 describe-vpcs --profile development --region "${region}" 2>&1 | jq '.Vpcs | .[] | .CidrBlock' | sed "s/^/\t/" )
+        vpc_cidr_count=$(aws ec2 describe-vpcs --profile development --region "${region}" 2>&1 | jq '.Vpcs | .[] | .CidrBlock' | wc -l)
         printf "%40s \n" " " | tr ' ' '.'
         echo "   ${region}"
         echo "    * EC2 Count: ${nodes_count}"
+        echo "${nodes}"
+        echo "    * VPC Count: ${vpc_cidr_count}"
+        echo "${vpc_cidr}"
         echo "    * Public IPs: ${public_ips}"
         echo "    * Network intefaces: ${network_intefaces}"
-        echo "${nodes}"
     fi
 done
