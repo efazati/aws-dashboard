b50be35f4ca00df8211773d952c7b8746542f615
Mohammad Efazati <mohammad@efazati.org>
2019-10-18 16:40:44 +0100

    Improve performance in EC2 to just use nodes list

--- services/ec2.sh
+++ services/ec2.sh
@@ -2,22 +2,21 @@ printf "%60s \n" " " | tr ' ' '-'
 echo "EC2:"
 for region in $(aws ec2 describe-instances --profile "${environment}"  2>&1 | jq '.Reservations[].Instances[].PrivateDnsName | split(".") | .[1]' | uniq | sed 's/\"//g'); \
 do 
-    nodes=$(aws ec2 describe-instances \
+    raw_nodes=$(aws ec2 describe-instances \
         --region "${region}" \
         --filter Name=instance-state-name,Values=running  --profile "${environment}"  \
         --query 'Reservations[].Instances[].{InstanceType:InstanceType}' \
-        | jq -r '.[].InstanceType' \
-        | sort \
-        | uniq -c \
-        | sort -r)
+        | jq -r '.[].InstanceType')
 
-    if  [ ! -z "$nodes" ]
+    if  [ ! -z "$raw_nodes" ]
     then
-        nodes_count=$(aws ec2 describe-instances \
-            --region "${region}" \
-            --filter Name=instance-state-name,Values=running  --profile "${environment}"  \
-            --query 'Reservations[].Instances[].{InstanceType:InstanceType}' \
-            | jq -r '.[].InstanceType' \
+        nodes=$(echo ${raw_nodes} \
+            | tr " " "\n" \
+            | sort \
+            | uniq -c \
+            | sort -r)
+        nodes_count=$(echo ${raw_nodes} \
+            | tr " " "\n" \
             | wc -l)
         network_intefaces=$(aws ec2 describe-network-interfaces --profile "${environment}" --region "${region}" 2>&1 | jq '.NetworkInterfaces | .[] | "\(.NetworkInterfaceId) -> \(.Description)"'  | wc -l)
         public_ips=$(aws ec2 describe-addresses --profile "${environment}" --region "${region}" 2>&1 | jq '.Addresses | .[] | .PublicIp' | wc -l)
