6bb101485b8963e5abd3c075343628f755f9bb16
Mohammad Efazati <mohammad@efazati.org>
2019-10-18 16:49:42 +0100

    Improve vpc performance

--- services/vpc.sh
+++ services/vpc.sh
@@ -2,11 +2,16 @@ printf "%60s \n" " " | tr ' ' '-'
 echo "VPC:"
 for region in $(aws ec2 describe-regions --profile "${environment}" --query 'Regions[].RegionName | sort(@)' --output text); \
 do 
-    vpc_cidr_count=$(aws ec2 describe-vpcs --profile development --region "${region}" 2>&1 | jq '.Vpcs | .[] | .CidrBlock' | wc -l)
+    raw_vpc_cidr=$(aws ec2 describe-vpcs --profile development --region "${region}" 2>&1 | jq '.Vpcs | .[] | .CidrBlock')
+    vpc_cidr_count=$(echo ${raw_vpc_cidr} \
+        | tr " " "\n" \
+        | wc -l)
 
     if  [ "${vpc_cidr_count}" -ne "1" ];
     then
-        vpc_cidr=$(aws ec2 describe-vpcs --profile development --region "${region}" 2>&1 | jq '.Vpcs | .[] | .CidrBlock' | sed "s/^/      /" )
+        vpc_cidr=$(echo ${raw_vpc_cidr} \
+            | tr " " "\n" \
+            | sed "s/^/      /" )
         printf "%40s \n" " " | tr ' ' '.'
         echo "   ${region}"
         echo "    * VPC Count: ${vpc_cidr_count}"
