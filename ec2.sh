22d40495d55058e01605af5697da908b8f32dcc0
Mohammad Efazati <mohammad@efazati.org>
2019-10-18 11:22:32 +0100

    Gather more detail related to EC2

--- ec2.sh
+++ ec2.sh
@@ -21,11 +21,14 @@ do
                 --query 'Reservations[].Instances[].{InstanceType:InstanceType}' \
                 | jq -r '.[].InstanceType' \
                 | wc -l)
-            network_intefaces=$(aws ec2 describe-network-interfaces --profile "${environment}" --region "${region}" -- 2>&1 | jq '.NetworkInterfaces | .[] | "\(.NetworkInterfaceId) -> \(.Description)"'  | wc -l)
+            network_intefaces=$(aws ec2 describe-network-interfaces --profile "${environment}" --region "${region}" 2>&1 | jq '.NetworkInterfaces | .[] | "\(.NetworkInterfaceId) -> \(.Description)"'  | wc -l)
+            public_ips=$(aws ec2 describe-addresses --profile "${environment}" --region "${region}" 2>&1 | jq '.Addresses | .[] | .PublicIp' | wc -l)
             echo "   ${region}"
-            echo "    * EC2 Counts: ${nodes_count}"
+            echo "    * EC2 Count: ${nodes_count}"
+            echo "    * Public IPs: ${public_ips}"
             echo "    * Network intefaces: ${network_intefaces}"
             echo "${nodes}"
         fi
     done
 done
+
